d832eab99bd69fa945f4eef6e79b307f6320cb5f
Vadym Nemchenko <westa.nvv@gmail.com>
2024-08-09 00:16:55 +0300

    Updated styles for a login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import "@app/globals.scss";
-import type { Metadata } from "next";
+import '@styles/globals.scss';
+import type {Metadata} from 'next';
 
 export const metadata: Metadata = {
-  title: "Nexar: Programming Task",
-  description: "Programming task for React.js interviews"
+  title: 'Nexar: Programming Task',
+  description: 'Programming task for React.js interviews',
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>{children}</body>
